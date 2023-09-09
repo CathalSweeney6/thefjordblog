887aa781fa5f741b6745ea42c1a987e21342c492
Cathal Sweeney <cjsweeney6@gmail.com>
2023-09-09 15:14:57 +0000

    Deployment commit

--- thefjord/settings.py
+++ thefjord/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thefjord.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['thefjord-b1491443270e.herokuapp.com', 'localhost']
 
 
 # Application definition
