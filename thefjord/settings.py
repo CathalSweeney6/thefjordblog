ab17973cb7595a84b6b4e226d259d554b0daa759
Cathal Sweeney <cjsweeney6@gmail.com>
2023-10-11 19:45:35 +0000

    Add Contact Us Form

--- thefjord/settings.py
+++ thefjord/settings.py
@@ -166,3 +166,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'https://formdump.codeinstitute.net/'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
