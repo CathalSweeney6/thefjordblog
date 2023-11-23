eeca727149d94974d2043bc6d9de12b475b11184
Cathal Sweeney <cjsweeney6@gmail.com>
2023-11-23 15:58:26 +0000

    correct additional pep8 errors

--- blog/models.py
+++ blog/models.py
@@ -54,7 +54,7 @@ class Comment(models.Model):
 class Contact(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
-    message = models.TextField(null=False, blank=False)    
+    message = models.TextField(null=False, blank=False)
 
     def __str__(self):
         return self.name
